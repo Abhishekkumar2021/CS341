1e911bbbb36a6bcb35741ceeea2032699b756b0d
Abhishekkumar2021 <abhishekku20022002@gmail.com>
2022-08-31 14:27:18 +0530

    Added some more folder

---
+++ 05/Problems/app.js
@@ -0,0 +1,134 @@
+questions = [
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "The word protocol is often used to describe diplomatic relations. How does Wikipedia describe diplomatic protocol?",
+        "ans": "<ul><li>The heads of countires (or) heads of nations represents the diplomatic protocol.</li><li>The word protocol contains set of rules to deals with other nations/countries.</li><li>It is a special type of art to guidig intraction between the different nations/countries.</li>"
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+    {
+        "ques": "What is the difference between a host and an end system? List several different types of end systems. Is a Web server an end system?",
+        "ans": "Host is Computer or a device connected to a network which provide information, resources or services to users or other host in a network. End Systems are termed as they are at the edge of internet which does not interact with the users but provide various services.For example, web servers and e-mail servers which does not interact with users directly but provide resources.End System are considered same as hosts but with no direct interaction with the user and Web Server is an end system."
+    },
+
+]
+
+let idx = 0;
+const sidebar = document.querySelector(".sidebar")
+for( let i=1; i<=questions.length; i++){
+    const div = document.createElement("button")
+    div.classList.add("btn")
+    div.innerHTML = i;
+    sidebar.append(div)
+}
+const ques = document.querySelector(".question")
+const ans = document.querySelector(".answer")
+const btns = document.querySelectorAll(".btn")
+for(let btn of btns){
+    btn.addEventListener("click",(e)=>{
+        let i = parseInt(e.target.innerHTML);
+        ques.innerHTML = questions[i-1].ques
+        ans.innerHTML = questions[i-1].ans
+    })
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# CS341
+# This repository provides you the solution to the book questions from
+`Computer Network - A top down approach`
+
+## How to contribute to its repository?
+### There are 9 chapters in this book and each chapers have two types of problems 
+- ### Review problems
+- ### Exercise problems
+### So what you have to is that you have to add the HTML for every question and answer text into the javascript file present in every problem section.
+
+### The questions and answer text can be get by using the link [Here](http://www.sr2jr.com/textbook-solutions/computer-science/10101001/computer-networking-a-top-down-approach-computer-networks-and-the-internet)
+`Thanks`
+
